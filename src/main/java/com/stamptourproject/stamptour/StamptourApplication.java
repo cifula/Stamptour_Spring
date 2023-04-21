18b1a750ac1596831291581d3a0e2d6d44e1a005
cifula <lidel95@gmail.com>
2023-04-21 14:43:05 +0900

    first commit

---
+++ src/main/java/com/stamptourproject/stamptour/StamptourApplication.java
@@ -0,0 +1,13 @@
+package com.stamptourproject.stamptour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamptourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StamptourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stamptourproject/stamptour/StamptourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stamptourproject.stamptour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StamptourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
